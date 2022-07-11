6aac6f1c13121ee053e549bf494998d694acd146
AntoniuMerla <64725704+AntoniuMerla@users.noreply.github.com>
2022-07-11 16:47:25 +0200

    Update bot.py
    here we go again

--- bot.py
+++ bot.py
@@ -5,18 +5,26 @@ import constants
 import googletrans
 from gnews import GNews
 from discord.ext import commands
-from discord.commands import OptionChoice
-from discord.commands import Option
+from discord import OptionChoice
+from discord import Option
 
 bot = commands.Bot(intents=discord.Intents.all(), help_command=None)
 
 
-@bot.slash_command(name='hi', description="Greets the user.", guild_ids=[os.environ.get('GUILD_ID')])
+@bot.slash_command(
+    name='hi',
+    description="Greets the user.",
+    guild_ids=[os.environ.get('GUILD_ID')]
+)
 async def _sayhi(ctx: discord.ApplicationContext):
     await ctx.respond(random.choice(constants.GREETINGS))
 
 
-@bot.slash_command(name='fcbnews', description="Shows actual news of FC Bayern.", guild_ids=[os.environ.get('GUILD_ID')])
+@bot.slash_command(
+    name='fcbnews',
+    description="Shows actual news of FC Bayern.",
+    guild_ids=[os.environ.get('GUILD_ID')]
+)
 async def _bayernmuenchenfacts(ctx: discord.ApplicationContext):
     google_news = GNews(max_results=10)
     bayernmuenchen_news = google_news.get_news('Bayern Munich match, transfer')
@@ -24,7 +32,6 @@ async def _bayernmuenchenfacts(ctx: discord.ApplicationContext):
     description = google_news.get_full_article(randomised_url).title
     embed = discord.Embed(title="Bayern Nachrichten!!!",
                           url=randomised_url,
-                          # url="https://fcbayern.com/en/news",
                           description=description,
                           color=discord.Color.blue())
     embed.set_thumbnail(
@@ -32,8 +39,16 @@ async def _bayernmuenchenfacts(ctx: discord.ApplicationContext):
     await ctx.respond(embed=embed)
 
 
-@bot.slash_command(name='translate', description="Translates the message into the selected language.", guild_ids=[os.environ.get('GUILD_ID')])
-async def _translate(ctx: discord.ApplicationContext, language: Option(str, "Enter language: en/de."), text: Option(str, "Enter text to translate.")):
+@bot.slash_command(
+    name='translate',
+    description="Translates the message into the selected language.",
+    guild_ids=[os.environ.get('GUILD_ID')]
+)
+async def _translate(
+    ctx: discord.ApplicationContext,
+    language: OptionChoice(value=["en", "de"]),
+    text: Option(str, "Enter text to translate.")
+):
     if language != "en" and language != "de":
         await ctx.respond("Sehe ich aus, als könnte ich Französisch sprechen?")
     else:
@@ -44,7 +59,11 @@ async def _translate(ctx: discord.ApplicationContext, language: Option(str, "Ent
         await ctx.respond(translated_text)
 
 
-@bot.slash_command(name='bavarian',description="Tells about bavarian life.",guild_ids=[os.environ.get('GUILD_ID')])
+@bot.slash_command(
+    name='bavarian',
+    description="Tells about bavarian life.",
+    guild_ids=[os.environ.get('GUILD_ID')]
+)
 async def _livelikeabavarian(ctx: discord.ApplicationContext):
     actual_list = list(constants.BAVARIAN_TO_DO_LIST)
     random_key = random.choice(actual_list)
@@ -54,7 +73,11 @@ async def _livelikeabavarian(ctx: discord.ApplicationContext):
     await ctx.respond(embed=embed)
 
 
-@bot.slash_command(name="commands", description="Sends all available commands.", guild_ids=[os.environ.get('GUILD_ID')])
+@bot.slash_command(
+    name="commands",
+    description="Sends all available commands.",
+    guild_ids=[os.environ.get('GUILD_ID')]
+)
 async def _help(ctx: discord.ApplicationContext):
     embed = discord.Embed(title=f'Available Commands:', description='\uFEFF',
                           colour=ctx.author.colour)
